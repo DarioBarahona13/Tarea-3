90af7a48318ec0d3ad32a288de52c881522fef2c
DarioBarahona13 <109933610+DarioBarahona13@users.noreply.github.com>
2022-08-07 23:47:15 -0600

    Add files via upload
    Tarea 3

---
+++ Documentacion.md
@@ -0,0 +1,9 @@
+# Documentacion Tarea 3
+
+*Aplicación web que permita administrar una base de datos de películas creada en javascript usando clases para poder ordenar la informacion.*
+
+---
+La informacion de las peliculas se obutvo de el siguiente enlace :
+
+> [imdb](https://www.imdb.com/title/tt13223398/?ref_=rlm "imdb")
+
---
+++ index.js
@@ -0,0 +1,61 @@
+class Blonde {
+
+    
+    constructor(Nombre_Pelicula,Categorias, Director, Escritor,Actor_Principal,Fecha_Salida) {
+      this.Nombre_Pelicula = Nombre_Pelicula
+      this.Categorias = Categorias
+      this.Director = Director
+      this.Escritor = Escritor
+      this.Actor_Principal = Actor_Principal
+      this.Fecha_Salida = Fecha_Salida
+    }
+
+  }
+
+  var Blonde1 = new Blonde("Blonde","Accion","Andrew Dominik","Andrew Dominik","Ana de armas", new Date(2022,9,28))
+ 
+  
+
+  console.log(Blonde1);
+  
+  
+
+  class Dragonball {
+
+    
+    constructor(Nombre_Pelicula,Categorias, Director, Escritor,Actor_Principal,Fecha_Salida) {
+      this.Nombre_Pelicula = Nombre_Pelicula
+      this.Categorias = Categorias
+      this.Director = Director
+      this.Escritor = Escritor
+      this.Actor_Principal = Actor_Principal
+      this.Fecha_Salida = Fecha_Salida
+    }
+
+  }
+
+  
+  var Dragon_ball1 = new Dragonball("Dragon Ball Super: Super Hero","Animacion-Aventura","Tetsuro Kodama","Tetsuro Kodama","Masako", new Date(2022,8,18))
+  
+  console.log(Dragon_ball1);
+  
+  
+
+  class Beast {
+
+    
+    constructor(Nombre_Pelicula,Categorias, Director, Escritor,Actor_Principal,Fecha_Salida) {
+      this.Nombre_Pelicula = Nombre_Pelicula
+      this.Categorias = Categorias
+      this.Director = Director
+      this.Escritor = Escritor
+      this.Actor_Principal = Actor_Principal
+      this.Fecha_Salida = Fecha_Salida
+    }
+
+  }
+
+  var Beast1 = new Beast("Beast","Drama-Horro","Baltasar Kormákur","Ryan Engle","Idris Elba", new Date(2022,8,22))
+  
+
+  console.log(Beast1);
